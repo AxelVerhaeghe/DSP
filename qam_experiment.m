0047a2db36f0ff2c8ee28d0e343d14d03c0190b9
Axel Verhaeghe <Verhaeghe.A.J@gmail.com>
2018-11-02 10:08:23 +0100

    added 32-QAM

--- qam_experiment.m
+++ qam_experiment.m
@@ -4,7 +4,7 @@ input = randi([0,1],1,len);
 snr1 = 10;
 snr2 = 20;
 
-% 4-QAM
+%% 4-QAM
 qam2 = qam_mod(input,2); %Modulating the signal without noise (4-QAM)
 qam2Noise1 = awgn(qam2,snr1); %Adding noise to the modulated signal
 qam2Noise2 = awgn(qam2,snr2);
@@ -18,7 +18,7 @@ fprintf(1,"BER for 4-QAM and SNR of %d = %f%%\n",snr1,100*ber21);
 ber22 = ber(input,output22);
 fprintf(1,"BER for 4-QAM and SNR of %d = %f%%\n",snr2,100*ber22);
 
-% 8-QAM
+%% 8-QAM
 qam3 = qam_mod(input,3); %Modulating the signal without noise (4-QAM)
 qam3Noise1 = awgn(qam3,snr1); %Adding noise to the modulated signal
 qam3Noise2 = awgn(qam3,snr2);
@@ -32,7 +32,7 @@ fprintf(1,"BER for 8-QAM and SNR of %d = %f%%\n",snr1,100*ber31);
 ber32 = ber(input,output32);
 fprintf(1,"BER for 8-QAM and SNR of %d = %f%%\n",snr2,100*ber32);
 
-% 16-QAM
+%% 16-QAM
 qam4 = qam_mod(input,4); %Modulating the signal without noise (4-QAM)
 qam4Noise1 = awgn(qam4,snr1); %Adding noise to the modulated signal
 qam4Noise2 = awgn(qam4,snr2);
@@ -46,7 +46,21 @@ fprintf(1,"BER for 16-QAM and SNR of %d = %f%%\n",snr1,100*ber41);
 ber42 = ber(input,output42);
 fprintf(1,"BER for 16-QAM and SNR of %d = %f%%\n",snr2,100*ber42);
 
-% 64-QAM
+%% 32-QAM
+qam5 = qam_mod(input,5); %Modulating the signal without noise (4-QAM)
+qam5Noise1 = awgn(qam5,snr1); %Adding noise to the modulated signal
+qam5Noise2 = awgn(qam5,snr2);
+scatterplot(qam5,1,0,'m+'); %Scatterplot of the signal without noise
+scatterplot(qam5Noise1,1,0,'m+'); %Scatterplot of the signal with noise
+scatterplot(qam5Noise2,1,0,'m+'); %Scatterplot of the signal with noise
+output51 = qam_demod(qam5Noise1,5); %Demodulating the noisy transmitted signal
+output52 = qam_demod(qam5Noise2,5);
+ber51 = ber(input,output51);
+fprintf(1,"BER for 32-QAM and SNR of %d = %f%%\n",snr1,100*ber51);
+ber52 = ber(input,output52);
+fprintf(1,"BER for 32-QAM and SNR of %d = %f%%\n",snr2,100*ber52);
+
+%% 64-QAM
 qam6 = qam_mod(input,6); %Modulating the signal without noise (4-QAM)
 qam6Noise1 = awgn(qam6,snr1); %Adding noise to the modulated signal
 qam6Noise2 = awgn(qam6,snr2);
