5f4d6fb45d8514920b075cf40fc3370a1e9ec09e
Axel Verhaeghe <Verhaeghe.A.J@gmail.com>
2018-11-04 17:40:53 +0100

    Accidentaly misplaced some files

--- slprj/ofdm_experiment.m
+++
@@ -1,6 +0,0 @@
-clear();
-len = 1800;
-dftSize = 64;
-input = randi([0,1],1,len);
-qam4 = qam_mod(input,4); %Modulating the signal without noise (4-QAM)
-ofdm = ofdm_mod(qam4,dftSize,40);
--- slprj/qam_demod.m
+++
@@ -1,9 +0,0 @@
-function y = qam_demod(signal,n)
-    M = 2^n;
-    signalSize = size(signal);
-    nbRows = signalSize(1);
-    demodSig = qamdemod(signal,M);
-    binDemodSig = de2bi(demodSig);
-    y = reshape(binDemodSig,1,nbRows*n);
-    
-end
