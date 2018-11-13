1a3af57325c8ebe7ea0ce239a6537f7ccb4856af
Axel Verhaeghe <Verhaeghe.A.J@gmail.com>
2018-11-13 17:54:18 +0100

    work on on/off keying

--- milestone 2/main.m
+++ milestone 2/main.m
@@ -1,11 +1,12 @@
 % Exercise session 4: DMT-OFDM transmission scheme
 
 clear();
-n = 3;
+n = 6;
 qamBlockSize = 500;
-snr = 10;
+snr = 20;
 channel = load('IR2.mat');
 channelImpulseResponse = channel.h;
+channelFrequencyResponse = fft(channelImpulseResponse);
 channelOrder = length(channelImpulseResponse);
 prefixLength = channelOrder*1.3;
 fs = 16000;
@@ -18,39 +19,49 @@ qamStream = qam_mod(bitStream,n);
 
 % OFDM modulation
 [ofdmStream,paddingSize] = ofdm_mod(qamStream,qamBlockSize,prefixLength);
+[ofdmStreamOnOff,paddingSizeOnOff,usableFrequencies] = ofdm_mod_onoff(qamStream,qamBlockSize,prefixLength,channelFrequencyResponse);
 ofdmStreamWithNoise = awgn(ofdmStream,snr,'measured');
+ofdmStreamWithNoiseOnOff = awgn(ofdmStreamOnOff,snr,'measured');
 
 % Channel
-% Random channel ht
-ht = ones(channelOrder,1);
+% Random channel ht1
+ht1 = ones(channelOrder,1);
 for i=1:channelOrder
-   ht(i) = rand(1); 
+   ht1(i) = rand(1); 
 end
 
-afterChannelRandom = fftfilt(ht,ofdmStreamWithNoise);
+afterChannelRandom1 = fftfilt(ht1,ofdmStreamWithNoise);
+afterChannelAcousticOnOff = fftfilt(channelImpulseResponse,ofdmStreamWithNoise);
 afterChannelAcoustic = fftfilt(channelImpulseResponse,ofdmStreamWithNoise);
 
 % OFDM demodulation
-rxQamStreamRandom = ofdm_demod(afterChannelRandom,qamBlockSize,prefixLength,paddingSize,ht);
+rxQamStreamRandom1 = ofdm_demod(afterChannelRandom1,qamBlockSize,prefixLength,paddingSize,ht1);
+rxQamStreamAcousticOnOff = ofdm_demod_onoff(afterChannelAcousticOnOff,qamBlockSize,prefixLength,paddingSize,channelImpulseResponse,usableFrequencies);
 rxQamStreamAcoustic = ofdm_demod(afterChannelAcoustic,qamBlockSize,prefixLength,paddingSize,channelImpulseResponse);
 
 % QAM demodulation
-rxBitStreamRandom = qam_demod(rxQamStreamRandom,n);
-rxBitStreamRandom = transpose(rxBitStreamRandom);
+rxBitStreamRandom1 = qam_demod(rxQamStreamRandom1,n);
+rxBitStreamRandom1 = transpose(rxBitStreamRandom1);
+
+rxBitStreamAcousticOnOff = qam_demod(rxQamStreamAcousticOnOff,n);
+rxBitStreamAcousticOnOff = transpose(rxBitStreamAcousticOnOff);
 
 rxBitStreamAcoustic = qam_demod(rxQamStreamAcoustic,n);
 rxBitStreamAcoustic = transpose(rxBitStreamAcoustic);
 
 % Compute BER
-berTransmissionRandom = ber(bitStream,rxBitStreamRandom);
+berTransmissionRandom1 = ber(bitStream,rxBitStreamRandom1);
+berTransmissionAcousticOnOff = ber(bitStream,rxBitStreamAcousticOnOff);
 berTransmissionAcoustic = ber(bitStream,rxBitStreamAcoustic);
 
 % Construct image from bitstream
-imageRxRandom = bitstreamtoimage(rxBitStreamRandom, imageSize, bitsPerPixel);
+imageRxRandom1 = bitstreamtoimage(rxBitStreamRandom1, imageSize, bitsPerPixel);
+imageRxAcousticOnOff = bitstreamtoimage(rxBitStreamAcousticOnOff, imageSize, bitsPerPixel);
 imageRxAcoustic = bitstreamtoimage(rxBitStreamAcoustic, imageSize, bitsPerPixel);
 
 % Plot images
 figure();
 subplot(2,2,1); colormap(colorMap); image(imageData); axis image; title('Original image'); drawnow;
-subplot(2,2,2); colormap(colorMap); image(imageRxRandom); axis image; title(['Received image (Random Channel)']); drawnow;
-subplot(2,2,3); colormap(colorMap); image(imageRxAcoustic); axis image; title(['Received image (Acoustic Channel (IR2))']); drawnow;
+subplot(2,2,2); colormap(colorMap); image(imageRxRandom1); axis image; title('Received image (Random Channel 1)'); drawnow;
+subplot(2,2,3); colormap(colorMap); image(imageRxAcousticOnOff); axis image; title('Received image (Acoustic Channel (on/off keying))'); drawnow;
+subplot(2,2,4); colormap(colorMap); image(imageRxAcoustic); axis image; title('Received image (Acoustic Channel (IR2))'); drawnow;
---
+++ milestone 2/ofdm_mod_onoff.m
@@ -0,0 +1,51 @@
+function [ofdm,paddingSize,usableFrequencies] = ofdm_mod_onoff(qamSignal,qamBlockSize,prefixLength,channelFrequencyResponse)
+%OFDM_MOD_ONOFF applies a OFDM to the given QAM input signal by
+%only using the frequencies that are the least surpressed in the channel.
+%Input arguments:
+% - qamSignal                   The input signal modulated with QAM
+% - qamBlockSize                The size of the blocks
+% - prefixLength                The length of the cyclic prefix
+% - channelFrequencyResponse    The frequency response of the channel
+%Output:
+% - ofdm                The ofdm modulated signal
+% - paddingSize         If the signal is not divisible by qamBlocksize, the
+% signal will be padded with zeros. This parameter gives the amount of
+% zeros added.
+% - usableFrequencies   The frequencies used where the channel doesn't
+% surpress the signal as much.
+
+    %Calculate the maximum frequencies of freq_respons
+    maxFrequency = abs(max(channelFrequencyResponse));
+    absFrequency = abs(channelFrequencyResponse(1:length(channelFrequencyResponse)));
+    
+    nbUsableFrequencies = qamBlockSize + 1; %initialising nbUsableFrequencies to be greater than qamBlockSize
+    tol = 0.5;
+    while (nbUsableFrequencies > qamBlockSize)
+        usableFrequencies = find(absFrequency>tol*maxFrequency);  
+        nbUsableFrequencies = length(usableFrequencies);
+        tol = tol + 0.1;
+    end
+    
+    qamSignal = transpose(qamSignal);
+    signalLength = length(qamSignal);
+    frameLength = 2*qamBlockSize+2;   
+    
+    % padding input with zeros
+    remainder = mod(signalLength,nbUsableFrequencies);
+    paddingSize = nbUsableFrequencies - remainder;
+    padding = zeros(1,paddingSize);
+    paddedQamSignal = [qamSignal,padding];
+    paddedSignalLength = length(paddedQamSignal);
+    numFrames = paddedSignalLength/nbUsableFrequencies;
+    
+    parallel = reshape(paddedQamSignal,qamBlockSize,numFrames);
+    frames = zeros(qamBlockSize,numFrames);
+    for i=1:nbUsableFrequencies
+       frames(usableFrequencies(i),:) =  parallel(i,:);
+    end
+    
+    withoutPrefix = [zeros(1,numFrames);frames;zeros(1,numFrames);flipud(conj(frames))];
+    withoutPrefixTime = ifft(withoutPrefix); %apply ifft
+    ofdm = [withoutPrefixTime(frameLength-prefixLength+1:frameLength,:);withoutPrefixTime]; %add cyclic prefix of length L
+    ofdm = ofdm(:).'; %reshapes the array into a column vector and then applies transpose
+end
