4f325fa7779429313a88b2fe8b0880c3b5075b77
Axel Verhaeghe <Verhaeghe.A.J@gmail.com>
2018-11-28 15:35:38 +0100

    debugging week 5

---
+++ milestone 3/alignIO.m
@@ -0,0 +1,14 @@
+function [outAligned]=alignIO(out,pulse)
+
+[acor,lag] = xcorr(pulse,out);
+
+[~,index] = max(abs(acor));
+lagDiff = lag(index);
+
+outSync = out(-lagDiff+1:end);
+
+pulseLength = length(pulse);
+
+outAligned = outSync(pulseLength-60:end);
+end
+
