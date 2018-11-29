96211c30f728a83bec19cd70248458ec98b8a56c
Axel Verhaeghe <Verhaeghe.A.J@gmail.com>
2018-11-29 13:03:30 +0100

    ex 7-1 finished

---
+++ milestone 4/DDequalization.m
@@ -0,0 +1,23 @@
+clear();
+
+n = 3;
+len = 1000*n;
+
+input = randi([0,1],1,len);
+Xk = qam_mod(input,n);
+Hk = 3 + 2i;
+Yk = fftfilt(Hk,Xk);
+
+delta = 0.001*Hk;
+mu = 0.4; %stepSize: greater stepsize increases speed of convergence (too big causes oscilations)
+alpha = 0.5;
+channelLength = 100;
+Wk = zeros(1,channelLength);
+Wk(1) = 1/conj(Hk) + delta;
+
+for L = 1:channelLength
+   Wk(L+1) = Wk(L) + mu/(alpha + conj(Yk(L+1))*Yk(L+1)) * Yk(L+1) * conj(Xk(L+1) - conj(Wk(L))*Yk(L+1));
+end
+
+figure();
+plot(abs(Wk)-abs(1/conj(Hk))); title('Convergence of adaptive filter');
